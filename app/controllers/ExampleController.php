b74f01f455e1329fc551cea81800f940030f13ff
togask <141380453+togask@users.noreply.github.com>
2023-12-01 14:58:31 +0900

    Controllerの仮組み

---
+++ app/controllers/ExampleController.php
@@ -0,0 +1,27 @@
+<?php
+class ExampleController
+{
+    public function search()
+    {
+        // GET /api/example に対する処理
+        // データの取得や処理のロジックをここに記述
+    }
+
+    public function create()
+    {
+        // POST /api/example に対する処理
+        // データの作成や保存のロジックをここに記述
+    }
+
+    public function update()
+    {
+        // PUT/PATCH /api/example に対する処理
+        // データの更新のロジックをここに記述
+    }
+
+    public function delete()
+    {
+        // DELETE /api/example に対する処理
+        // データの削除のロジックをここに記述
+    }
+}
