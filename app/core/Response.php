fed09f066f92a6e48e95e51de67cc1409591ca28
togask <141380453+togask@users.noreply.github.com>
2023-12-01 16:38:45 +0900

    Responseの作成

---
+++ app/core/Response.php
@@ -0,0 +1,32 @@
+<?php
+class Response
+{
+  public static function sendResponse($statusCode, $data = null)
+  {
+    header("HTTP/1.1 " . $statusCode . " " . self::getStatusCodeMessage($statusCode));
+
+    if ($statusCode == 200) {
+      // HTTPステータスコードが200の場合、JSONデータを送信
+      header('Content-Type: application/json');
+      echo json_encode($data);
+    } elseif ($statusCode >= 400 && $statusCode < 500) { // HTTPステータスコードが400台の場合、エラーメッセージを送信
+      header('Content-Type: application/json');
+      echo json_encode(['error' => self::getStatusCodeMessage($statusCode)]);
+    }
+
+    exit;
+  }
+
+  private static function getStatusCodeMessage($code)
+  {
+    $statusCodes = [
+      200 => 'OK',
+      201 => 'Created',
+      204 => 'No Content',
+      400 => 'Bad Request',
+      401 => 'Unauthorized',
+      404 => 'Not Found',
+    ];
+    return (isset($statusCodes[$code])) ? $statusCodes[$code] : 'Error';
+  }
+}
