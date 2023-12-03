d61d281d5709eaa84542be6aff85b0470515c2de
togask <141380453+togask@users.noreply.github.com>
2023-12-03 18:31:09 +0900

    Session周りのメソッド

--- app/core/SessionManager.php
+++ app/core/SessionManager.php
@@ -3,11 +3,39 @@ class SessionManager
 {
   public static function startSession()
   {
-    // セッションを開始するロジック
+    if (session_status() == PHP_SESSION_NONE) {
+      session_start();
+    }
   }
 
   public static function checkSessionId()
   {
-    // セッションIDを確認するロジック
+    return isset($_SESSION['user_id']);
+  }
+
+  public static function set($key, $value)
+  {
+    $_SESSION[$key] = $value;
+  }
+
+  public static function get($key)
+  {
+    return $_SESSION[$key] ?? null;
+  }
+
+  public static function remove($key)
+  {
+    unset($_SESSION[$key]);
+  }
+
+  public static function destroySession()
+  {
+    session_unset();
+    session_destroy();
+  }
+
+  public static function regenerateSessionId($deleteOldSession = false)
+  {
+    session_regenerate_id($deleteOldSession);
   }
 }
