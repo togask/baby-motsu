936d8139d7f1bb850b06d9c35c1ac6879e900831
togask <141380453+togask@users.noreply.github.com>
2023-12-05 12:51:29 +0900

    GET /api/auth/loginの主要部分

--- app/core/Response.php
+++ app/core/Response.php
@@ -26,6 +26,7 @@ class Response
       400 => 'Bad Request',
       401 => 'Unauthorized',
       404 => 'Not Found',
+      500 => 'Internal Server Error'
     ];
     return (isset($statusCodes[$code])) ? $statusCodes[$code] : 'Error';
   }
